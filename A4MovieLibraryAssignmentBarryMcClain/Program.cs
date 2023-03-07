85df658cab8f38b28b897a24064a84190445bfd9
mcclain113 <barrymcclainrx@gmail.com>
2023-03-06 21:41:43 -0600

    Initial commit

---
+++ A4MovieLibraryAssignmentBarryMcClain/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using CsvHelper;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System.Globalization;
+using System.Linq;
+using A4MovieLibraryAssignmentBarryMcClain.Services;
+using CsvHelper.Configuration;
+using CsvHelper.Configuration.Attributes;
+
+namespace A4MovieLibraryAssignmentBarryMcClain
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            
+            //What I am moving to dependency injection:
+            /*MovieApp movieapp = new MovieApp();
+            MenuGenerator generator = new MenuGenerator();
+            generator.MovieGenerator(movieapp);*/
+
+            var startup = new Startup();
+            var serviceProvider = startup.ConfigureServices();
+            var service = serviceProvider.GetService<IMainService>();
+            service?.Invoke();
+            
+
+        }
+
+    }
+}   
